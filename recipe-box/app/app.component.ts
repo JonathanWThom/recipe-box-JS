323a97c59e227343091f71e90e08b330f1babc00
Tracie Weitzman <tlweitzman@gmail.com>
2017-01-17 11:04:48 -0800

    add recipe titles to page

--- recipe-box/app/app.component.ts
+++ recipe-box/app/app.component.ts
@@ -5,10 +5,21 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>Recipe Box</h1>
+    <ul>
+      <li *ngFor="let currentRecipe of recipes">{{currentRecipe.title}}</li>
+    </ul>
   </div>
   `
 })
 
 export class AppComponent {
+  recipes: Recipe[] = [
+    new Recipe ("Meatballs", "Beef, Veal, love", "pat them together and cook"),
+    new Recipe ("Fluffy French Toast", "Bread, Eggs, Syrup, Half and Half, Oil, Flour, Cinnamon", "Fry it up, man"),
+    new Recipe ("Cinnamon Rolls", "Flour, Milk, Yeast, Sugar, Cinnamon, Raisins, Walnuts", "Bake it up, yo")
+  ];
+}
 
+export class Recipe {
+  constructor(public title: string, public ingredients: string, public description: string) { }
 }
