83d8a0d652ed9670660ad2961db9f29c9a201e94
Tracie Weitzman <tlweitzman@gmail.com>
2017-01-17 11:49:24 -0800

    Add property binding for recipe priority

--- recipe-box/app/app.component.ts
+++ recipe-box/app/app.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
   <div class="container">
     <h1>Recipe Box</h1>
 
-    <div *ngFor="let currentRecipe of recipes">
-      <h4 (click)="showIngredients(currentRecipe)">{{currentRecipe.title}}</h4>
+    <div [class]="priorityColor(currentRecipe)" *ngFor="let currentRecipe of recipes">
+      <h4 (click)="isTested(currentRecipe)">{{currentRecipe.title}}</h4>
       <ul>
         <li>{{currentRecipe.ingredients}}</li>
         <li>{{currentRecipe.description}}</li>
@@ -20,23 +20,34 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   recipes: Recipe[] = [
-    new Recipe ("Meatballs", "Beef, Veal, love", "pat them together and cook"),
-    new Recipe ("Fluffy French Toast", "Bread, Eggs, Syrup, Half and Half, Oil, Flour, Cinnamon", "Fry it up, man"),
-    new Recipe ("Cinnamon Rolls", "Flour, Milk, Yeast, Sugar, Cinnamon, Raisins, Walnuts", "Bake it up, yo")
+    new Recipe ("Meatballs", "Beef, Veal, love", "pat them together and cook", 1),
+    new Recipe ("Fluffy French Toast", "Bread, Eggs, Syrup, Half and Half, Oil, Flour, Cinnamon", "Fry it up, man", 3),
+    new Recipe ("Cinnamon Rolls", "Flour, Milk, Yeast, Sugar, Cinnamon, Raisins, Walnuts", "Bake it up, yo", 2)
   ];
 
   editRecipe() {
     alert("You just requested to edit a Recipe");
   }
 
-  showIngredients(clickedRecipe: Recipe) {
-    if(clickedRecipe.ingredients){
-      alert(clickedRecipe.ingredients);
+  isTested(clickedRecipe: Recipe) {
+    if(clickedRecipe.tested === true){
+      alert("You've tried this one!");
     } else
-      alert("No ingredients listed yet");
+      alert("You haven't tried this recipe yet!");
+  }
+
+  priorityColor(currentRecipe) {
+    if (currentRecipe.priority === 3){
+      return "bg-danger";
+    } else if (currentRecipe.priority === 2){
+      return "bg-warning";
+    } else {
+      return "bg-success";
+    }
   }
 }
 
 export class Recipe {
-  constructor(public title: string, public ingredients: string, public description: string) { }
+  public tested: boolean = false;
+  constructor(public title: string, public ingredients: string, public description: string, public priority: number) { }
 }
